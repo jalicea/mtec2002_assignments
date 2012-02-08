36b04e572f7fe275e7ce08774287df1b163997da
jalicea <jonfreakintasic@gmail.com>
2012-02-08 13:50:45 -0500

    Update class1/labs/lpthw_ex5.py

---
+++ class1/labs/lpthw_ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Jon'
+my_age = 18 # not a lie
+my_height = 74 # inches 
+my_weight = 180 # lbs 
+my_eyes = 'Brown'
+my_hair = 'brown'
+my_teeth = 'white'
+
+print "lets talk about %s." % my_name
+print "He's %d inches tall." % my height
+print " he's %d pounds heavey." % my_weight
+print "Actually that's not too heavy."
+print "He gots %s eyes and %s hair." %( my_eyes, my_hair)
+
+print "If I add %d, %d, and %d I get %d." %(
+my_age, my_height, my_weight, my_age + my_height + my_weight) 
