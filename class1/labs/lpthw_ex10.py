a5f99635850ec0eb13ca5310bb5685e2383823d4
jalicea <jonfreakintasic@gmail.com>
2012-02-08 15:05:03 -0500

    Update class1/labs/lpthw_ex10.py

---
+++ class1/labs/lpthw_ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backkslash_cat = "Im \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food 
+\t* Cat food 
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
