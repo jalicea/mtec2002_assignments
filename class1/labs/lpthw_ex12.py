87008a75f6978378bcd94ba6358082d5f38bf56a
jalicea <jonfreakintasic@gmail.com>
2012-02-08 15:18:49 -0500

    Update class1/labs/lpthw_ex12.py

---
+++ class1/labs/lpthw_ex12.py
@@ -0,0 +1,6 @@
+age = raw_input("How old are you? ")
+height = raw_input("how tall are you? ")
+weight = raw_input("how much do you weoght? ")
+
+print "So, you're %r old, %r talland %r heavy."%(
+  age, height, weight)
